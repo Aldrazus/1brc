3acbeffdcb2705469ce95ee66659f77f181d1fa9
Alberto Espinal <aespinal59@gmail.com>
2024-01-10 13:03:31 -0500

    memory mapped io and views

--- src/calculate_average.cpp
+++ src/calculate_average.cpp
@@ -12,11 +12,18 @@
 #include <print>
 #include <chrono>
 
+#define NOMINMAX // lol ok
+#include <Windows.h>
+#include <memoryapi.h>
+
 /*  Changelist
  *  
- *  Initial impl:  1mm - 1.172s
+ *  Initial impl:                       1mm - 1.172s    |   1b - too long
+ *  Memory-mapped I/O + views/ranges:   1mm - 0.841s    |   1b - 6.381s
  *  
  */
+
+const int_fast32_t num_lines = 1000000;
 
 // TODO: check these types
 struct Stats {
@@ -27,26 +34,65 @@ struct Stats {
 };
 
 int main() {
+
+    HANDLE file = CreateFile(
+        "measurements_mm.txt",
+        GENERIC_READ,
+        FILE_SHARE_READ,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+
+    HANDLE mapping = CreateFileMapping(
+        file,
+        NULL,
+        PAGE_READONLY,
+        0,
+        0,
+        "measurements"
+    );
+
+    LARGE_INTEGER file_size;
+    GetFileSizeEx(
+            file,
+            &file_size
+    );
+
+    const char* mapped = reinterpret_cast<const char*>(MapViewOfFile(
+        mapping,
+        FILE_MAP_READ, // TODO: experiment with large pages
+        0,
+        0,
+        file_size.QuadPart
+    ));
+
+    std::string_view sv{mapped};
+
+    // split da line yaya
+    auto lines = sv
+        | std::views::split('\n')
+        | std::views::transform([](auto&& str) { return std::string_view(str); })
+        | std::views::take(num_lines - 1); // can we avoid this or make something more robust?
+
     std::map<std::string, Stats> stats;
-    
-    std::ifstream ifs{"measurements_mm.txt"};
 
-    std::ofstream ofs{"test.txt"};
-
-    if (!ifs) {
-        std::cerr << "Cannot open measurements.txt\n";
-    }
-
-    std::string line;
-    while (std::getline(ifs, line)) {
-        auto delim = line.find(';');
-        std::string id = line.substr(0, delim);
-        int_fast32_t measurement = std::stod(line.substr(delim + 1, std::string::npos)) * 10;
+    for (auto [i, line]: std::views::enumerate(lines)) {
+        // processLine(line);
+        std::string line_s{line};
+        auto delim = line_s.find(';');
+        std::string id = line_s.substr(0, delim);
+        int_fast32_t measurement = std::stod(line_s.substr(delim + 1, std::string::npos)) * 10;
+        try {
+        } catch (std::exception e) {
+            std::println("wtf is this shit {}: {}", id, line_s.substr(delim + 1, std::string::npos));
+        }
 
         if (!stats.contains(id)) {
             stats[id] = Stats{.min=std::numeric_limits<int_fast32_t>::max(), .total=0, .max=std::numeric_limits<int_fast32_t>::min(), .n=0};
         }
-        
+
         stats[id].max = std::max(stats[id].max, measurement);
         stats[id].min = std::min(stats[id].min, measurement);
         stats[id].n++;
