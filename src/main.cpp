88d10bee15dfe730c44989c07148764a2cea090f
Alberto Espinal <aespinal59@gmail.com>
2025-03-02 12:06:14 -0500

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,11 @@
 #include <string_view>
 #include <algorithm>
 #include <numeric>
+#include <utility>
+#include <thread>
+#include <filesystem>
+#include <unordered_map>
+#include <ranges>
 
 struct Statistics {
     int_fast16_t min = std::numeric_limits<int_fast16_t>::max();
@@ -31,7 +36,63 @@ int_fast16_t parseTemperature(std::string_view temp) {
     return sign * (100 * temp[0] + 10 * temp[1] + temp[3] - '0' * 111);
 }
 
+std::vector<std::pair<size_t, size_t>> splitFileIntoChunks(const std::string& file_path) {
+    const auto fileSize = std::filesystem::file_size(file_path);
+
+    const auto numChunks = std::thread::hardware_concurrency();
+
+    std::vector<std::pair<size_t, size_t>> chunks;
+    chunks.resize(numChunks);
+
+    std::println("filesize: {}", fileSize);
+    const auto baseChunkSize = fileSize / numChunks;
+    const auto remainder = fileSize % numChunks;
+
+    for (int i = 0; i < numChunks; i++) {
+        const auto chunkSize = baseChunkSize + (i < remainder ? 1 : 0);
+        const size_t start = i == 0 ? 0 : chunks[i-1].second;
+        const size_t end = start + chunkSize;
+        chunks[i] = {start, end};
+    }
+
+    return chunks;
+}
+
+std::unordered_map<std::string, Statistics> processChunk(std::string_view data, size_t start, size_t end) {
+    std::unordered_map<std::string, Statistics> stats;
+
+    for (; start != 0 && data[start] != '\n'; start++) {}
+
+    for (end--; end < data.size() && data[end] != '\n'; end++) {}
+
+    const auto chunk = data.substr(start, end - start);
+
+    auto lines = chunk | std::views::split('\n') | std::views::transform([](auto&& str) { return std::string_view{str}; });
+
+    for (auto&& line : lines) {
+        const char delim = ';';
+
+        const auto delimIdx = line.find(delim);
+        const auto name = line.substr(0, delimIdx);
+        const auto temp = parseTemperature(std::string_view(line).substr(delimIdx + 1));
+
+        auto& [min, max, n, total] = stats[std::string(name)];
+        min = std::min(min, temp);
+        max = std::max(max, temp);
+        n++;
+        total += temp;
+    }
+
+    return stats;
+}
+
 int main() {
+    const auto chunks = splitFileIntoChunks("data/measurements.txt");
+    
+    for (const auto& [start, end] : chunks) {
+        std::println("{} to {}", start, end);
+    }
+
     std::ifstream file("data/measurements.txt");
     if (!file) {
         std::println(std::cerr, "Error opening file!");
